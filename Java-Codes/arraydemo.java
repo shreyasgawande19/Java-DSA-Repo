1c77342ad15d2a6914174c472a38a2fa7015b88a
Shreyas Gawande <shreyasgawande19@gmail.com>
2026-05-09 10:43:33 +0530

    Added new code in Java of Arrays

---
+++ Java-Codes/arraydemo.java
@@ -0,0 +1,49 @@
+import java.util.*;
+class arraydemo{
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //Implementing 1-D ARRAYS
+    //     int arr[] = new int[5];
+    //     for(int i=0; i<arr.length; i++){
+    //         arr[i] = sc.nextInt();
+    //     }
+    //     for(int i=0; i<arr.length; i++){
+    //         System.out.println(arr[i]);
+    //         String []arr= new String[4];
+    //         for(int i=0; i<arr.length;i++){
+    //             arr[i]=sc.next();
+    //         }
+    //        System.out.println(Arrays.toString(arr));
+
+    //        arr[1]="Shreyas";
+    //        System.out.println(Arrays.toString(arr));
+        //Passing Array in a FUNCTION
+    //     int [] num= {1,2,3,4,5};
+    //     System.out.println(Arrays.toString(num));
+
+    //     change(num);
+    //     System.out.println(Arrays.toString(num));
+
+    // }
+
+    // static void change(int []arr){
+    //         arr[0]=99;
+
+    ////Implementing 2-D ARRAYS
+    //         int arr[][]=new int[3][3];
+    //         for(int i=0;i<arr.length;i++){
+    //             for(int j=0;j<arr[i].length;j++){
+    //                 arr[i][j]=sc.nextInt();
+    //             }
+    //         }
+            //Using Exception Handling
+    //         try{
+    //             for(int i=0;i<arr[i].length;i++){
+    //             System.out.println(Arrays.toString(arr[i]));
+    //         }
+    //         }
+    //         catch(ArrayIndexOutOfBoundsException e){
+    //             System.out.print("Exception Occurred");
+    //         }
+        }
+}
