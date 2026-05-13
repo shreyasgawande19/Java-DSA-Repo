5117d7129d167fb16e429452b7b4111e7858834b
Shreyas Gawande <shreyasgawande19@gmail.com>
2026-05-13 19:58:18 +0530

    Added pattern Java file

---
+++ Java-Codes/patterns.java
@@ -0,0 +1,37 @@
+class patterns{
+        static void pattern1(){
+             for(int i=1; i<=4; i++){
+            for(int j=1; j<=4;j++){
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+            static void pattern2(){
+            for(int a=1; a<=5;a++){
+                for(int b=1; b<=a;b++){
+                    System.out.print(b);
+                }
+                System.out.println();
+            }
+        }
+         static void pattern3(){
+            for(int c=1; c<=5;c++){
+                for(int d=1; d<=c;d++){
+                    System.out.print(c + " ");
+                }
+                System.out.println();
+            }
+        }
+    public static void main(String[]args){
+        // pattern1();
+        // pattern2();
+        // pattern3();
+
+    }
+}
+
+
+
+
+   
