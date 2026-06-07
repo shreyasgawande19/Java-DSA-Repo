282dbfa43cd95c6e94e8cfa8b44452709232087e
Shreyas Gawande <shreyasgawande19@gmail.com>
2026-06-07 18:55:58 +0530

    add binary search code

---
+++ Arrays/binarysearch.java
@@ -0,0 +1,28 @@
+
+public class binarysearch {
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int target = 12;
+        int ans = bsearch(array, target);
+        System.out.println(ans);
+    }
+
+    static int bsearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
