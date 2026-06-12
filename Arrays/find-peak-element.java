42a0632a653b74819ffe38c5111e1de9ff57c9e5
Shreyas Gawande <shreyasgawande19@gmail.com>
2026-06-12 20:43:23 +0530

    commited

---
+++ Arrays/find-peak-element.java
@@ -0,0 +1,20 @@
+
+class findpeakelement {
+
+    public static void main(String args[]) {
+
+    }
+
+    static int find(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            }
+        }
+    }
